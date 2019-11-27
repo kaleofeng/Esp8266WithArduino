f6deab29548bd0630d8409fbd1fb0b80d6fba958
KaleoFeng <kaleofeng@gmail.com>
2019-11-27 15:55:33 +0800

    Use 4-pin oled

--- WebConfigWifi/WebConfigWifi.ino
+++ WebConfigWifi/WebConfigWifi.ino
@@ -60,7 +60,8 @@ const char httpsFingerprint[] PROGMEM = "98 55 01 22 54 27 b0 22 7c 7b de 33 2e 
 String realIp;
 String realPort;
 
-U8G2_SSD1306_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 5, /* data=*/ 4, /* cs=*/ 15, /* dc=*/ 12, /* reset=*/ 13);
+//U8G2_SSD1306_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 5, /* data=*/ 4, /* cs=*/ 15, /* dc=*/ 12, /* reset=*/ 13);
+U8G2_SSD1306_128X64_NONAME_1_SW_I2C u8g2(U8G2_R0, /* clock=*/ 5, /* data=*/ 4, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
   Serial.begin(115200);
